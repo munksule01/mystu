67209b937b437851d8d18ca93cf5329332610ee3
munksule01 <munk_sule01@hotmail.com>
2026-05-01 15:08:25 +0100

    List part 2: Exercise time

--- core_list.py
+++ core_list.py
@@ -10,14 +10,21 @@ and it also allow duplicates
 
 thisList = ['Apple', 'Banana', 'Cherry', 'Mango']
 
+thisList[1:3] = ["watermelon"]
+
+thisList.insert(2, 'Grapes')
+print(thisList)
 print(type(thisList))
 
 myList = ['Apple', 4, True, False, 0, 6j, 'Banana', 'Mango']
 
 myList[4] = 'Cherry'
+myList.insert(3, 'Grapes')
 
 print(len(myList))
 print(myList)
+
+myList[3:9] = ['Grapes', 'Pineapple', 'Watermelon', 'Strawberry', 'Blueberry', 'Raspberry']
 print(myList[3:9])
 
 
@@ -27,3 +34,26 @@ elif 'Cherry'in thisList:
     print('Yes, Cherry is in the list(thisList)')
 else:
     print('No, Cherry is not in the list');
+
+
+theList = ['Apple', 'Banana', 'Cherry', 'Mango']
+HeList = ['Pineapple', 'Strawberry', 'Blueberry', 'Raspberry']
+thisTuple = ('Ama', 'Kofi', 'Kwame', 'Esi')
+
+theList.append('Yellow Melon')
+HeList.append('Dawadawa')
+
+
+print(theList)
+print(HeList)
+print(thisTuple)
+
+theList.extend(thisTuple)
+print(theList)
+
+HeList.extend(theList)
+print(HeList)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
