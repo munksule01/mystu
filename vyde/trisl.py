3724ee46d31261e38b02cc1f0df7d14008dc5713
munksule01 <munk_sule01@hotmail.com>
2026-05-04 21:44:57 +0100

    I have finally sorted it great work to myself

--- vyde/trisl.py
+++ vyde/trisl.py
@@ -1,16 +1,15 @@
 def user_api():
 
     users = []
-    user = []
 
     for i in range(3):
         username = input("Please enter your username : ")
-        password = input("Please enter your password : ")    
+        password = input("Please enter your password : ")
+        user = []  
         user.append(username)
         user.append(password)
         users.append(user)
-        user = []  # Reset the user list for the next iteration
-        
+
     print(users)
 
 user_api()
